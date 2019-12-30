3ab4b7ca69e8ee2f9d46bca6a609ae7271569717
NAFerrero <nicolasferreroutn@gmail.com>
2019-12-30 15:14:35 -0300

    arreglos tabla puntuaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { environment } from '../environments/environment';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { environment } from "../environments/environment";
+import { LayoutModule } from "@angular/cdk/layout";
 
 // Mis componentes
-import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
-import { PrincipalComponent } from './pages/principal/principal.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegistroComponent } from './pages/registro/registro.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { TablaComponent } from './pages/puntuacion/tabla/tabla.component';
-import { PuntuacionComponent } from './pages/puntuacion/puntuacion/puntuacion.component';
-import { UserService } from './services/user.service';
+import { NavBarComponent } from "./pages/nav-bar/nav-bar.component";
+import { PrincipalComponent } from "./pages/principal/principal.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegistroComponent } from "./pages/registro/registro.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { TablaComponent } from "./pages/puntuacion/tabla/tabla.component";
+import { PuntuacionComponent } from "./pages/puntuacion/puntuacion/puntuacion.component";
+import { UserService } from "./services/user.service";
 
 // Mis juegos
-import { MemoryGameComponent } from './pages/juegos/memory-game/memory-game.component';
-import { AdivinarNumeroGameComponent } from './pages/juegos/adivinar-numero-game/adivinar-numero-game.component';
-import { AgilidadMentalGameComponent } from './pages/juegos/agilidad-mental-game/agilidad-mental-game.component';
-import { AnagramaGameComponent } from './pages/juegos/anagrama-game/anagrama-game.component';
-import { PptGameComponent } from './pages/juegos/ppt-game/ppt-game.component';
-import { BoardComponent } from './pages/juegos/tateti-game/board/board.component';
-import { SquareComponent } from './pages/juegos/tateti-game/square/square.component';
-import { PerfilComponent } from './pages/perfil/perfil.component';
+import { MemoryGameComponent } from "./pages/juegos/memory-game/memory-game.component";
+import { AdivinarNumeroGameComponent } from "./pages/juegos/adivinar-numero-game/adivinar-numero-game.component";
+import { AgilidadMentalGameComponent } from "./pages/juegos/agilidad-mental-game/agilidad-mental-game.component";
+import { AnagramaGameComponent } from "./pages/juegos/anagrama-game/anagrama-game.component";
+import { PptGameComponent } from "./pages/juegos/ppt-game/ppt-game.component";
+import { BoardComponent } from "./pages/juegos/tateti-game/board/board.component";
+import { SquareComponent } from "./pages/juegos/tateti-game/square/square.component";
+import { PerfilComponent } from "./pages/perfil/perfil.component";
 
 // Angular material modules
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatTableModule } from "@angular/material/table";
+import { MatSortModule } from "@angular/material/sort";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatIconModule } from "@angular/material/icon";
 
 // Firebase modules
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireAuthGuard } from "@angular/fire/auth-guard";
 
 @NgModule({
   declarations: [
@@ -65,10 +66,11 @@ import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
     SquareComponent,
     TablaComponent,
     PuntuacionComponent,
-    PerfilComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
+    LayoutModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
@@ -87,9 +89,9 @@ import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     AngularFireAuthModule,
-    AngularFireStorageModule,
+    AngularFireStorageModule
   ],
   providers: [AngularFireAuthGuard, UserService],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/pages/puntuacion/puntuacion/puntuacion.component.ts
+++ src/app/pages/puntuacion/puntuacion/puntuacion.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { UserService } from 'src/app/services/user.service';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
+import { Subscription } from "rxjs";
+import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-puntuacion',
-  templateUrl: './puntuacion.component.html',
-  styleUrls: ['./puntuacion.component.css'],
+  selector: "app-puntuacion",
+  templateUrl: "./puntuacion.component.html",
+  styleUrls: ["./puntuacion.component.css"]
 })
 export class PuntuacionComponent implements OnInit, OnDestroy {
   datos: any;
   suscripcion: Subscription;
-  constructor(private userService: UserService) {}
+  hideTable = false;
+  constructor(
+    private userService: UserService,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   ngOnInit() {
+    this.breakpointObserver
+      .observe(["(min-width: 560px)"])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.hideTable = false;
+        } else {
+          this.hideTable = true;
+        }
+      });
     this.suscripcion = this.userService.getUsers().subscribe(users => {
       this.datos = users;
     });
